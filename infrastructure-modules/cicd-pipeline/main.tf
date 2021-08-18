0f23a4090aa278e1daefede8b0f33aef926afead
Mohamed KEITA <medesko@gmail.com>
2021-08-18 13:15:35 +0200

    define cicd pipeline code build and code pipeline

---
+++ infrastructure-modules/cicd-pipeline/main.tf
@@ -0,0 +1,20 @@
+data "aws_secretsmanager_secret" "github_secret" {
+  name = var.github_secret_name
+}
+
+data "aws_secretsmanager_secret_version" "github_token" {
+  secret_id = data.aws_secretsmanager_secret.github_secret.id
+}
+
+module "frontend_and_cicd_pipeline" {
+  source = "./resources"
+  application_name = var.application_name
+  s3_bucket_destination = "${var.application_name}-${var.environment}"
+  pipeline_bucket_name = "${var.application_name}-codepipeline"
+  codebuild_bucket_name = "${var.application_name}-codebuild"
+  github_org = var.github_org_name
+  repository_name = var.repository_name
+  branch_name = var.branch_name
+  environment = var.environment
+  github_token = jsondecode(data.aws_secretsmanager_secret_version.github_token.secret_string)["GITHUB_ACCESS_TOKEN"]
+}
---
+++ infrastructure-modules/cicd-pipeline/variables.tf
@@ -0,0 +1,40 @@
+variable "profile" {
+  description = "AWS profile"
+  type        = string
+}
+
+variable "region" {
+  description = "aws region to deploy stack"
+  type        = string
+}
+
+variable "application_name" {
+  description = "The name of the application"
+  type        = string
+}
+
+variable "environment" {
+  description = "Applicaiton environment"
+  type        = string
+}
+
+variable "github_org_name" {
+  description = "Github organization name"
+  type        = string
+  default     = "medesko"
+}
+
+variable "github_secret_name" {
+  type   = string
+}
+
+variable "repository_name" {
+  description = "Git repository name"
+  type        = string
+  default     = "terraform-react-app-deployments-aws-s3-cloudfront"
+}
+
+variable "branch_name" {
+  description = "Git branch name"
+  type        = string
+}
