c61574addb29a7cab885890a9b5ecf3e16d640b3
Mohamed KEITA <medesko@gmail.com>
2021-08-20 12:11:32 +0200

    aws_iam_policy_document codebuild_frontend

--- infrastructure-modules/cicd-pipeline/resources/iam_statement.tf
+++ infrastructure-modules/cicd-pipeline/resources/iam_statement.tf
@@ -77,3 +77,57 @@ resource "aws_iam_role_policy_attachment" "codebuild_frontend" {
   role       = aws_iam_role.codebuild_frontend.name
   policy_arn = aws_iam_policy.codebuild_frontend.arn
 }
+
+data "aws_iam_policy_document" "codebuild_frontend" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "ec2:CreateNetworkInterface",
+      "ec2:DescribeDhcpOptions",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DeleteNetworkInterface",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeVpcs",
+      "ec2:CreateNetworkInterfacePermission"
+    ]
+
+    resources = ["arn:aws:logs:eu-west-1:*","arn:aws:ec2:eu-west-1:*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:*"
+    ]
+
+    resources = [
+      "arn:aws:s3:::*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "codebuild:UpdateReportGroup",
+      "codebuild:ListReportsForReportGroup",
+      "codebuild:CreateReportGroup",
+      "codebuild:CreateReport",
+      "codebuild:UpdateReport",
+      "codebuild:ListReports",
+      "codebuild:DeleteReport",
+      "codebuild:ListReportGroups",
+      "codebuild:BatchPutTestCases"
+    ]
+
+    resources = [
+      "arn:aws:codebuild:eu-west-1:*"
+    ]
+  }
+}
