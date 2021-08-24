195c98f0b291faa0a379e118358bfc330fc15e57
Mohamed KEITA <medesko@gmail.com>
2021-08-24 17:33:28 +0200

    added demo app frontend

---
+++ frontend-app/src/components/organisms/user/UserDetailModal.tsx
@@ -0,0 +1,107 @@
+import { ChangeEvent, memo, useEffect, useState, VFC } from "react";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Stack
+} from "@chakra-ui/react";
+
+import { User } from "../../../types/api/user";
+import { PrimaryButton } from "../../atoms/button/PrimaryButton";
+
+type Props = {
+  user: User | null;
+  isOpen: boolean;
+  isAdmin?: boolean;
+  onClose: () => void;
+};
+
+export const UserDetailModal: VFC<Props> = memo((props) => {
+  const { user, isOpen, isAdmin = false, onClose } = props;
+
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  console.log(name)
+  useEffect(() => {
+    setUsername(user?.username ?? "");
+    setName(user?.name ?? "");
+    setEmail(user?.email ?? "");
+    setPhone(user?.phone ?? "");
+  }, [user]);
+
+  const onChangeUserName = (e: ChangeEvent<HTMLInputElement>) =>
+    setUsername(e.target.value);
+  const onChangeName = (e: ChangeEvent<HTMLInputElement>) =>
+    setName(e.target.value);
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) =>
+    setEmail(e.target.value);
+  const onChangePhone = (e: ChangeEvent<HTMLInputElement>) =>
+    setPhone(e.target.value);
+
+  const onClickUpdate = () => alert();
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      autoFocus={false}
+      motionPreset="slideInBottom"
+    >
+      <ModalOverlay />
+      <ModalContent pb={2}>
+        <ModalHeader>User details</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody mx={4}>
+          <Stack spacing={4}>
+            <FormControl>
+              <FormLabel>Name</FormLabel>
+              <Input
+                value={username}
+                onChange={onChangeUserName}
+                isReadOnly={!isAdmin}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Full Name</FormLabel>
+              <Input
+                value={username}
+                onChange={onChangeName}
+                isReadOnly={!isAdmin}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Mail</FormLabel>
+              <Input
+                value={email}
+                onChange={onChangeEmail}
+                isReadOnly={!isAdmin}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Phone</FormLabel>
+              <Input
+                value={phone}
+                onChange={onChangePhone}
+                isReadOnly={!isAdmin}
+              />
+            </FormControl>
+          </Stack>
+        </ModalBody>
+        {isAdmin && (
+          <ModalFooter>
+            <PrimaryButton onClick={onClickUpdate}>Up date</PrimaryButton>
+          </ModalFooter>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+});
